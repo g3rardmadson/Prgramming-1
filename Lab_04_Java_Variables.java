c7d298c194151bbf77ff9697dcb4497638fb003c
g3rardmadson <madsonar@mail.uc.edu>
2025-09-22 12:09:05 -0400

    Update Lab_04_Java_Variables.java

--- Lab_04_Java_Variables.java
+++ Lab_04_Java_Variables.java
@@ -1,3 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    
+    public static void main(String[] args) {
+        int ntOperandA = 12;
+        int intOperandB = 5;
+        int intSum = ntOperandA+ intOperandB;
+        System.out.println("The sum of " + ntOperandA+ " and "+ intOperandB +" is " + intSum);
+        int intProduct = ntOperandA*intOperandB;
+        System.out.println("The product of " + ntOperandA+ " and "+ intOperandB +" is " + intProduct);
+        int intDifference = ntOperandA-intOperandB;
+        System.out.println("The difference of " + ntOperandA+ " and "+ intOperandB +" is " + intDifference);
+        int intQuotient = ntOperandA/intOperandB;
+        System.out.println("The difference of " + ntOperandA+ " and "+ intOperandB +" is " + intQuotient);
+        int intModulo = ntOperandA%intOperandB;
+        System.out.println("The modulo of " + ntOperandA+ " and "+ intOperandB +" is " + intModulo);
+
+        double doubleOperandA = 3.7;
+        double doubleOperandB = 3.9;
+        double doubleSum = ntOperandA+ intOperandB;
+        System.out.println("The sum of " + doubleOperandA+ " and "+ doubleOperandB +" is " + doubleSum);
+        double doubleProduct = ntOperandA-intOperandB;
+        System.out.println("The product of " + doubleOperandA+ " and "+ doubleOperandB +" is " + doubleProduct);
+        double doubleDifference = ntOperandA-intOperandB;
+        System.out.println("The difference of " + doubleOperandA+ " and "+ doubleOperandB +" is " + doubleDifference);
+        double doubleQuotient = ntOperandA/doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA+ " and "+ doubleOperandB +" is " + doubleDifference);
+        double doubleModulo = ntOperandA%intOperandB;
+        System.out.println("The modulo of " + doubleOperandA+ " and "+ doubleOperandB +" is " + doubleDifference);
+        double lunchPrice = 7.0;
+        int numChildren = 2;
+        boolean raining = true;
+        double priceofGas = 3.0;
+        int favoriteNumber = 21;
+        int shoeSize = 9;
+        Strng myBirthMonth = "June";
+        String myName = "Gerard Rose Madson-Clemen";
+    }
 }
